63534288f229ea9f4c7dfe8e2163cc9a86f43fdf
skibidi sigma <tonyqianshijin@gmail.com>
2026-02-20 18:40:39 +0000

    Commit

---
+++ Python/mySchedule.py
@@ -0,0 +1,14 @@
+period_a = "Chemistry" 
+period_b = "Math"
+period_c = "English"
+period_d = "Art"
+period_e = "Spanish"
+period_f = "History"
+period_g = "Dialogues"
+period_specials = "Special"
+
+print("Monday schedule: " + period_c + ", "+ period_specials + ", " + period_a + ", " + period_b)
+print("Tuesday schedule: " + period_f + ", " + period_e + ", " + period_g + ", " + period_d)
+print("Wednsday schedule: " + period_g + ", " + period_c + ", " + period_e + ", " + period_b + ", " + period_a)
+print("Thursday schedule: " + period_b + ", " + period_f + ", " + period_d + ", " + period_a + ", " + period_g)
+print("Friday schedule: " + period_e + ", " + period_d + ", " + period_f + ", " + period_g + ", " + period_c)
