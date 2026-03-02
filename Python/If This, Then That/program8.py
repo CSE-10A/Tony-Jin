a380335803a0ee8b36e53c0d24a8b5048da53592
skibidi sigma <tonyqianshijin@gmail.com>
2026-03-02 19:40:17 +0000

    Filled in some code

--- Python/If This, Then That/program8.py
+++ Python/If This, Then That/program8.py
@@ -7,12 +7,15 @@ If n is even and in the inclusive range of  6 to, 10 print Weird
 If n is even and greater than 20, print Not Weird
 """
 
-n = int(input("Enter an integer: "))
+# note: this code is missing the condition for even numbers below 1 and between 11 and 20, since the problem statement does not specify what to do in those cases.
 
-if n % 2 != 0:
+n = int(input("Enter an integer: ")) # integer variable
+
+if n % 2 != 0: # check if n is odd
     print("Weird")
-elif n >= 2 and n <= 5:
+elif n >= 2 and n <= 5: # n between 2 and 5?
     print("Not Weird")
-elif n >= 6 and n <= 10:
+elif n >= 6 and n <= 10: # n between 6 and 10?
     print("Weird")
-elif n >= 20
+elif n > 20: # n greater than 20?
+    print("Not Weird")
--- Python/If This, Then That/program9.py
+++ Python/If This, Then That/program9.py
@@ -12,3 +12,4 @@ Task
 
 Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
 """
+def is_leap(year):
