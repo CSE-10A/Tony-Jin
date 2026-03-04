c12b59547327ca1fedf0f3acf9381daa3de96bf8
skibidi sigma <tonyqianshijin@gmail.com>
2026-03-04 14:18:33 +0000

    finalized random name generator

--- Python/If This, Then That/march4.py
+++ Python/If This, Then That/march4.py
@@ -1,4 +1,5 @@
 import random
+
 names = ["arjun", "jeri", "totti", "ethan", "jack", "peter", "nicholas", "tony", "david", "ollie", "earl", "andrew", "alex", "devon", "emmanuel jeffers"]
 random_var = random.randint(0, len(names) - 1)
 print(names[random_var])
