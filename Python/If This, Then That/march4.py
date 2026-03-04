78276e2069c7901db4489e4e91d0ca9cb9bba169
skibidi sigma <tonyqianshijin@gmail.com>
2026-03-04 14:16:59 +0000

    fixed bug

--- Python/If This, Then That/march4.py
+++ Python/If This, Then That/march4.py
@@ -1,4 +1,4 @@
 import random
 names = ["arjun", "jeri", "totti", "ethan", "jack", "peter", "nicholas", "tony", "david", "ollie", "earl", "andrew", "alex", "devon", "emmanuel jeffers"]
-random_var = randint(0, len(names) - 1)
+random_var = random.randint(0, len(names) - 1)
 print(names[random_var])
